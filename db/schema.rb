45d216fdc2bc4b557d1171fb404291edbd2b07ff
AliHaidar322 <bcsf19a020@pucit.edu.pk>
2025-01-07 17:22:20 +0500

    added reviews and showed the average rating

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_30_144251) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_01_081005) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,25 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_30_144251) do
     t.datetime "updated_at", null: false
     t.integer "price_cents"
     t.string "price_currency"
+    t.integer "reviews_count"
+    t.decimal "average_final_rating"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.float "cleanliness_rating"
+    t.float "accuracy_rating"
+    t.float "checkin_rating"
+    t.float "communication_rating"
+    t.float "location_rating"
+    t.float "value_rating"
+    t.float "final_rating"
+    t.bigint "user_id", null: false
+    t.bigint "property_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["property_id"], name: "index_reviews_on_property_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
